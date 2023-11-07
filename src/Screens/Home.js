eb6a9be8dccc2ef6d01247a25384b3fd83d5965a
Muhammad Mohsin <mohsinraja4667@gmail.com>
2023-11-07 11:18:29 +0530

    initial commit

---
+++ src/Screens/Home.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="flex h-full bg-gray-200 items-center justify-center">
+      <Text className="text-red-700 text-xl font-bold">Home Page</Text>
+    </View>
+  );
+};
+
+export default Home;
